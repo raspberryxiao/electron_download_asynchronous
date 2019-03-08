d1839022bd5bbc9a5dff22e3c8de0eab829aa66d
“liuyixiao” <liuyixiao@codemao.cn>
2019-03-08 18:30:16 +0800

    1

---
+++ appconfig.js
@@ -0,0 +1,11 @@
+module.exports ={
+    DOWNLOAD_TYPE: { 
+        ARDUINO_BUILDER: 1, 
+        ESP_TOOLS: 2,
+        EMPTY: 3,
+    },
+    zip_remote_path:{
+        // arduino_builder_zip: 'http://localhost/hardware/agent/resources/arduino-builder.mac.zip'
+        arduino_builder_zip: 'http://172.16.5.121/resources/codemao-hardware-agent-1.3.0.deb'
+    }
+}
